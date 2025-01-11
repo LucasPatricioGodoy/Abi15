431de12a98cd9fc450dde74333875b8f45dc8f5b
LPGODOY <LPGODOY@trabajo.gob.ar>
2025-01-10 22:04:47 -0300

    Agregar archivos de proyecto.

---
+++ Abi15/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const btnContinuar = document.getElementById("btnContinuar");
+    const overlay = document.getElementById("bienvenida");
+
+    btnContinuar.addEventListener("click", function () {
+        overlay.classList.add("hidden"); // Ocultamos el overlay
+    });
+
+    // Observador de las secciones
+    const sections = document.querySelectorAll('.section');
+    const options = {
+        root: null, // Usa el viewport como contenedor
+        rootMargin: '0px',
+        threshold: 0.2 // Se activa cuando el 20% de la sección es visible
+    };
+
+    const observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section); // Observa cada sección
+    });
+});
